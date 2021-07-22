a730bcb6b379e25e0b495b6e79f5e04ce0957b2c
glekovkirilll <glekovkirill2016@gmail.com>
2021-07-22 13:20:57 +0700

    Добавлена функция считывания числа с проверкой на корректность введённых данных

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,8 +3,25 @@ import java.util.Scanner;
 
 public class Main {
 
-    public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
+    static Scanner in = new Scanner(System.in);
 
+    public static double enterNumber(char a) {
+        double number;
+        System.out.println("Введите число " + a + ": ");
+
+        if(!in.hasNextDouble()) {
+            System.out.println("Введено некорректное число! Попробуйте снова: ");
+            in.next();
+            number = enterNumber(a);
+        }
+        else {
+            number = in.nextDouble();
+        }
+        return number;
+    }
+
+    public static void main(String[] args) {
+        double num1 = enterNumber('a');
+        System.out.println(num1);
     }
 }
