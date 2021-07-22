f1098818f7b6fd9f18cfb3137f0b97029449a51d
glekovkirilll <glekovkirill2016@gmail.com>
2021-07-22 14:26:38 +0700

    Добавлена функция считывания арифметического знака с проверкой на корректность введённых данных

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,23 +5,44 @@ public class Main {
 
     static Scanner in = new Scanner(System.in);
 
-    public static double enterNumber(char a) {
+    public static double inputNumber(char index) {
         double number;
-        System.out.println("Введите число " + a + ": ");
+        System.out.println("Введите число " + index + ": ");
 
         if(!in.hasNextDouble()) {
-            System.out.println("Введено некорректное число! Попробуйте снова: ");
+            System.out.println("Введено некорректное число! Попробуйте снова...");
             in.next();
-            number = enterNumber(a);
+            number = inputNumber(index);
         }
         else {
             number = in.nextDouble();
         }
         return number;
     }
+    public static char inputSign() {
+        char sign;
+        System.out.println("Введите знак: ");
+
+        if(in.hasNext()) {
+            sign = in.next().charAt(0);
+            if(sign == '+' || sign == '-' || sign == '*' || sign == '/') {
+                return sign;
+            }
+            else {
+                System.out.println("Введен некорректный знак! Попробуйте снова...");
+                sign = inputSign();
+
+            }
+        }
+        else {
+            System.out.println("Введен некорректный знак! Попробуйте снова...");
+            sign = inputSign();
+        }
+        return sign;
+    }
 
     public static void main(String[] args) {
-        double num1 = enterNumber('a');
-        System.out.println(num1);
+        char sign = inputSign();
+        System.out.println(sign);
     }
 }
